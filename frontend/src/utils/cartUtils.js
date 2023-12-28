b06e53d0befd468fd6da0ed48e6c585d45f425b7
Arman <armanbarseghyan83@gmail.com>
2023-12-28 09:49:03 -0800

    cartUtils.js refactored

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -1,27 +1,21 @@
-// check this file maybe dont need addDecimals, only tofix and dont need to repeat Number,
-// check maybe dont need return here and when thid func used
-//*************************************************************** */
-export const addDecimals = (num) => {
-  return (Math.round(num * 100) / 100).toFixed(2);
-};
-
 export const updateCart = (state) => {
   // Calculate the items price
-  state.itemsPrice = addDecimals(
-    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  state.itemsPrice = state.cartItems.reduce(
+    (acc, item) => acc + item.price * item.qty,
+    0
   );
 
   // Calculate the shipping price
-  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+  state.shippingPrice = state.itemsPrice > 100 ? 0 : 10;
 
   // Calculate the tax price
-  state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
+  state.taxPrice = (0.15 * state.itemsPrice).toFixed(2);
 
   // Calculate the total price
   state.totalPrice = (
-    Number(state.itemsPrice) +
-    Number(state.shippingPrice) +
-    Number(state.taxPrice)
+    +state.itemsPrice +
+    +state.shippingPrice +
+    +state.taxPrice
   ).toFixed(2);
 
   // Save the cart to localStorage
